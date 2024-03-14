e7466f0f10823e737c042c84b3f773d6d89cd8c7
Hellogaeun <lge05230@naver.com>
2024-03-14 18:00:39 +0900

    fourth commit

--- 이름은.py
+++
@@ -1,3 +0,0 @@
-input = 4.0
-print("Input;",input)
-name = input("이름은?")
--- 청소년할인.py
+++
@@ -1,5 +0,0 @@
-age = int(input("몇살이세요?:"))
-if age < 20 :
-    print("청소년 할인 대상입니다.")
-else :
-        print("나이가 너무 많아요...")
